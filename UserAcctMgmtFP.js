159228dffd5ee25604d0d82601acb0e9e907a17b
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-10-17 12:26:06 -0500

    updated export stuff, added test file to folder

--- UserAcctMgmtFP.js
+++ UserAcctMgmtFP.js
@@ -1,17 +1,17 @@
-module.exports = {
-	user: function(email, password, passwordConfirm) {
-		this.email = email;
-		this.password = password;
-		this.passwordConfirm = passwordConfirm;
-		return this;
-	}, //private
+const user = function(email, password, passwordConfirm) {
+	this.email = email;
+	this.password = password;
+	this.passwordConfirm = passwordConfirm;
+	return this;
+}; //private
 
-	signUp: function(obj) {
-		console.log(`${obj.email} ||| ${obj.password} ||| ${obj.passwordConfirm}`);
-	}, //private
-	signIn: function(obj) {
-		console.log("sign in happened");
-	},
-	signOut: function(obj) {},
-	changePassword: function changePassword(previous, next, obj) {}
+const signUp = function(obj) {
+	console.log(`${obj.email} ||| ${obj.password} ||| ${obj.passwordConfirm}`);
 };
+const signIn = function(obj) {
+	console.log("sign in happened");
+};
+const signOut = function(obj) {};
+const changePassword = function changePassword(previous, next, obj) {};
+
+module.exports = { user, signUp, signIn, signOut, changePassword };
